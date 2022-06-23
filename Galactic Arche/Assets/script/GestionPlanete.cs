5ad10b4e2b43e301a3b9d7b088cdcdddc152c988
Mlou15 <44841446+Mlou15@users.noreply.github.com>
2022-06-23 12:24:02 +0200

    Menu
    Ajout script pour le menu, Changer l'interface du menu (Ajout de play + quit)

---
+++ Galactic Arche/Assets/script/GestionPlanete.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GestionPlanete : MonoBehaviour
+{
+   public Sprite[] Splanetes;
+    public Carte[] Cartes;
+
+    
+}
